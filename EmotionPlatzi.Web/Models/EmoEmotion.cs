70597f306894bee73da0601584ae8cac0626da0b
Rafael Carmona <racv2005@gmail.com>
2018-05-18 05:22:01 -0500

    commit initial

---
+++ EmotionPlatzi.Web/Models/EmoEmotion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmotionPlatzi.Web.Models
+{
+    public class EmoEmotion
+    {
+        public int Id { get; set; }
+        public float Score { get; set; }
+        public int EmoFaceId { get; set; }
+        public EmoEmotionEnum EmotionType { get; set; }
+        public virtual EmoFace Faces { get; set; }
+    }
+}
---
+++ EmotionPlatzi.Web/Models/EmoPicture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace EmotionPlatzi.Web.Models
+{
+    public class EmoPicture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public virtual ObservableCollection<EmoFace> Faces { get; set; }
+
+
+    }
+}
